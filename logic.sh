84ab99bda3c686883dd6013c13a852201c7499f7
Farnas-Utsho <129610498+Farnas-Utsho@users.noreply.github.com>
2024-03-15 23:18:42 +0600

    Add files via upload

--- logic.sh
+++ logic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 age=30
-if [ "$age" -gt 18 ] && [ "$age" -lt 60 ] # if [[ "$age" -gt 18  &&  "$age" -lt 60  ]]
+if [ "$age" -gt 18 ] && [ "$age" -lt 60 ] # if [[ "$age" -gt 18  &&  "$age" -lt 60  ]] 
 then 
  echo " Valid age "
  else
@@ -9,7 +9,7 @@ then
 fi
 
 
-if [ "$age" == 18 ] || [ "$age" == 30 ]
+if [ "$age" == 18 ] || [ "$age" == 30 ] # [ "$age" -gt 18  -o  "$age" -lt 60  ] && [[ "$age" -gt 18  \\  "$age" -lt 60  ]]
 then 
  echo " Valid age "
  else
