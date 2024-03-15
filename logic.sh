80cc550248630d72d1cbe519d69b0760b07f897f
Farnas-Utsho <129610498+Farnas-Utsho@users.noreply.github.com>
2024-03-15 21:47:58 +0600

    Add files via upload
    logical operation

---
+++ logic.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+age=30
+if [ "$age" -gt 18 ] && [ "$age" -lt 60 ]
+then 
+ echo " Valid age "
+ else
+ echo " Not valid age " 
+fi
+
+
+if [ "$age" == 18 ] || [ "$age" == 30 ]
+then 
+ echo " Valid age "
+ else
+ echo " Not valid age " 
+fi
